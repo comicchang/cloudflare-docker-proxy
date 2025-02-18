02141057cad950f001856835a1ed238505be8243
Comic Chang <comicchang@gmail.com>
2025-02-18 15:56:32 +0800

    match route by prefix instead of full URL
    Signed-off-by: Comic Chang <comicchang@gmail.com>

--- src/index.js
+++ src/index.js
@@ -7,22 +7,24 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker"]: dockerHub,
+  ["quay"]: "https://quay.io",
+  ["gcr"]: "https://gcr.io",
+  ["k8s-gcr"]: "https://k8s.gcr.io",
+  ["k8s"]: "https://registry.k8s.io",
+  ["ghcr"]: "https://ghcr.io",
+  ["cloudsmith"]: "https://docker.cloudsmith.io",
+  ["ecr"]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging"]: dockerHub,
 };
 
 function routeByHosts(host) {
-  if (host in routes) {
-    return routes[host];
+  // extract prefix from host
+  const prefix = host.split(".")[0];
+  if (prefix in routes) {
+    return routes[prefix ];
   }
   if (MODE == "debug") {
     return TARGET_UPSTREAM;
